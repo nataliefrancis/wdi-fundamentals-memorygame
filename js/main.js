01f3af1acb07d4c730280ac640aad7df91b658d7
Natalie Francis <natdfrancis@gmail.com>
2017-08-24 22:17:49 -0600

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
 var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2]
+var cardsInPlay = []; 
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
+var checkForMatch = function () {
+	if (cardsInPlay [0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
+	}
+};
 
-console.log("user flipped " + cardOne);
-console.log("user flipped " + cardTwo);
+var flipCard = function (cardId) {	
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId])
+	
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+};
 
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else alert("Sorry, try again.");
-}
+flipCard(0);
+flipCard(2);
+
+
+
